e08692fe46a0cf39237916414312b29a3c58e479
Cloud Mu <muyunsong@gmail.com>
2016-08-23 18:08:55 +0800

    Add etag support.

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var express = require('express');
-var jsonServer = require('json-server');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -9,32 +8,9 @@ var bodyParser = require('body-parser');
 var routes = require('./routes/index');
 var users = require('./routes/users');
 var upp = require('./routes/upp');
+var sample = require('./routes/sample');
 
-// var app = express();
-var app = jsonServer.create();
-var sample = jsonServer.router(require('./routes/sample.js')());
-sample.render = function (req, res) {
-  console.log(typeof res.locals.data)
-  if (res.locals.data.length) {
-    res.jsonp({
-      respHeader: {
-        respCode: 'UPP-10000'
-      },
-      list: res.locals.data,
-      total: 130
-    });
-  } else {
-    res.jsonp({
-      respHeader: {
-        respCode: 'UPP-10000'
-      },
-      data: res.locals.data,
-    });
-  }
-}
-var middlewares = jsonServer.defaults();
-
-app.use(middlewares);
+var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
@@ -51,45 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', routes);
 app.use('/users', users);
 app.use('/upp', upp);
-app.post('/sample/web/v1/sso/getResources', function (req, res, next) {
-  res.send({
-    "aasUserPrincipal": {
-      "aasUserResources": {
-        "resources": [{
-          "levelStructure": "1.0.0",
-          "linkUrl": "/sample",
-          "resourceDisplayName": "样例工程",
-          "resourceType": "menu"
-        }, {
-          "levelStructure": "1.1.0",
-          "linkUrl": "/sample/blank-page.html",
-          "parentLevelStructure": "1.0.0",
-          "resourceDisplayName": "空白页面",
-          "resourceType": "menu"
-        }, {
-          "levelStructure": "1.2.0",
-          "linkUrl": "/sample/list-page.html",
-          "parentLevelStructure": "1.0.0",
-          "resourceDisplayName": "列表页面",
-          "resourceType": "menu"
-        }, {
-          "levelStructure": "1.2.1",
-          "linkUrl": "/sample/detail-page.html",
-          "parentLevelStructure": "1.2.0",
-          "resourceDisplayName": "详情页面",
-          "resourceType": "menu"
-        }],
-        "roles": [{
-          "roleName": "Administrator"
-        }]
-      },
-      "ssoUserName": "Cloud Mu"
-    },
-    "respHeader": {
-      "respCode": "AAS-10000"
-    }
-  });
-});
 app.use('/sample/web/v1', sample);
 
 // catch 404 and forward to error handler
